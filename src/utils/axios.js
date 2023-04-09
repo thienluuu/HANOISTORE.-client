cbe8c7b674c978478214d3c4666241c36465b816
CrissNguyen <thienm199x@gmail.com>
2023-04-09 21:54:32 +0700

    n

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "https://server-hanoistore.onrender.com",
 });
 
 // request.interceptors.response.use((response) => {
