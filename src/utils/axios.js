5fc58d7336ec7b7b055a116787ee8234e342b040
CrissNguyen <thienm199x@gmail.com>
2023-04-09 20:41:50 +0700

    last update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+require("dotenv").config();
 
 const request = axios.create({
-  baseURL: "https://server-hanoistore.onrender.com",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 // request.interceptors.response.use((response) => {
